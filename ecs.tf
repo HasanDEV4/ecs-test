e54e0150f092924c835d883e52a1cc6a9472cc34
Hassan Ali Jauhar <91012251+HasanDEV4@users.noreply.github.com>
2025-01-08 18:47:23 +0500

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,8 +1,9 @@
+# ECS Cluster Declaration
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "my-ecs-cluster"
 }
 
-# ALB
+# ALB (Application Load Balancer)
 resource "aws_lb" "app_lb" {
   name               = "app-load-balancer"
   internal           = false
@@ -17,14 +18,12 @@ resource "aws_lb" "app_lb" {
   }
 }
 
-
 # ALB Target Group
 resource "aws_lb_target_group" "app_tg" {
   name     = "app-target-group"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.dev_vpc.id # VPC ID refer kar rahe hain
-  target_type = "ip"  # Yeh line add karen
 
   health_check {
     path                = "/"
@@ -51,7 +50,6 @@ resource "aws_lb_listener" "app_listener" {
 resource "aws_security_group" "lb_sg" {
   vpc_id = aws_vpc.dev_vpc.id # VPC ID refer kar rahe hain
   
-
   ingress {
     from_port   = 80
     to_port     = 80
