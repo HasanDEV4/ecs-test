c48154a494435ec0fa3d61bda02ddb58d96c0d08
Hassan Ali Jauhar <91012251+HasanDEV4@users.noreply.github.com>
2025-01-08 18:09:22 +0500

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -20,7 +20,7 @@ resource "aws_lb_target_group" "app_tg" {
   name     = "app-target-group"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.dev_vpc.id
+  vpc_id   = aws_vpc.dev_vpc.id # VPC ID refer kar rahe hain
 
   health_check {
     path                = "/"
@@ -45,7 +45,7 @@ resource "aws_lb_listener" "app_listener" {
 
 # Security Group for ALB
 resource "aws_security_group" "lb_sg" {
-  vpc_id = aws_vpc.dev_vpc.id
+  vpc_id = aws_vpc.dev_vpc.id # VPC ID refer kar rahe hain
 
   ingress {
     from_port   = 80
