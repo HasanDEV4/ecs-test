b5ba914dda3f71e6a52070695bfc5536826b9f7b
Hassan Ali Jauhar <91012251+HasanDEV4@users.noreply.github.com>
2025-01-08 18:35:22 +0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -17,6 +17,18 @@ resource "aws_subnet" "public_subnet" {
   }
 }
 
+# Public Subnet 2
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.dev_vpc.id
+  cidr_block              = "10.0.102.0/24"  # Different CIDR block for the second subnet
+  availability_zone       = "us-east-1b"  # Different Availability Zone
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.dev_vpc.id
   cidr_block        = "10.0.1.0/24"
