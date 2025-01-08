929a0423d8b3c0f379603f091a3a509f976c1a87
HasanDEV4 <jauharhassan4@gmail.com>
2025-01-08 17:36:00 +0500

    first push

---
+++ ecr.tf
@@ -0,0 +1,12 @@
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name                 = "my-app-repo" # Repository ka naam
+  image_tag_mutability = "MUTABLE"     # Optional, images ka tag mutable ya immutable
+  image_scanning_configuration {
+    scan_on_push = true # Push ke waqt image scanning enable karne ke liye
+  }
+}
+
+output "ecr_repository_url" {
+  description = "URL of the ECR Repository"
+  value       = aws_ecr_repository.my_ecr_repo.repository_url
+}
---
+++ ecs.tf
@@ -0,0 +1,106 @@
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "my-ecs-cluster"
+}
+
+# ALB
+resource "aws_lb" "app_lb" {
+  name               = "app-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.public_subnet.id]
+
+  tags = {
+    Name = "app-load-balancer"
+  }
+}
+
+# ALB Target Group
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.dev_vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# ALB Listener
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+# Security Group for ALB
+resource "aws_security_group" "lb_sg" {
+  vpc_id = aws_vpc.dev_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "lb-sg"
+  }
+}
+
+# ECS Task Definition
+resource "aws_ecs_task_definition" "nginx_task" {
+  family                   = "nginx-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  container_definitions = jsonencode([{
+    name      = "nginx"
+    image     = "nginx:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
+}
+
+# ECS Service
+resource "aws_ecs_service" "nginx_service" {
+  name            = "nginx-service"
+  cluster         = aws_ecs_cluster.ecs_cluster.id
+  task_definition = aws_ecs_task_definition.nginx_task.arn
+  desired_count   = 3
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = [aws_subnet.private_subnet.id]
+    security_groups = [aws_security_group.lb_sg.id]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app_tg.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
+}
